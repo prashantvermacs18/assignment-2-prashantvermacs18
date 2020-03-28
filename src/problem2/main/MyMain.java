35b82f5759196ea15e86cba945b532bf141b9099
prashantvermacs18 <prashant.verma_cs18@gla.ac.in>
2020-03-28 18:51:14 +0530

    print the data of node in postfix manner

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -13,6 +13,28 @@ import problem1.node.TreeNode;
 
 public class MyMain {
     public static void main(String[] args) {
+        public static void main(String[] args) {
+            MyBinarySearchTree<Integer> myBinarySearchTree = new MyBinarySearchTree<>();
+            myBinarySearchTree.add(10);
+            myBinarySearchTree.add(17);
+            myBinarySearchTree.add(8);
+            myBinarySearchTree.add(15);
+            myBinarySearchTree.add(1);
 
+            System.out.println("Traverse the tree in PreOrder :");
+            traversePreOrder(myBinarySearchTree.getRoot());
+
+            System.out.println();
+
+            System.out.println("Traverse the tree in PostOrder :");
+            traversePostOrder(myBinarySearchTree.getRoot());
+        }
+        public static void traversePreOrder(TreeNode<Integer> currentNode) {
+            if (currentNode != null) {
+                System.out.print(currentNode.getData() + " ");
+                traversePreOrder(currentNode.getLeftChild());
+                traversePreOrder(currentNode.getRightChild());
+            }
+        }
     }
 }
