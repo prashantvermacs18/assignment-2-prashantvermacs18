1a2b2222d6518bb0fa82339baacc2f6c969840d2
prashantvermacs18 <prashant.verma_cs18@gla.ac.in>
2020-03-28 18:40:07 +0530

    Adding integers

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,22 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree<Integer> myBinarySearchTree = new MyBinarySearchTree<>();
+        myBinarySearchTree.add(100);
+        myBinarySearchTree.add(50);
+        myBinarySearchTree.add(200);
+        myBinarySearchTree.add(25);
+        myBinarySearchTree.add(75);
+        myBinarySearchTree.add(150);
+        myBinarySearchTree.add(300);
+        myBinarySearchTree.add(40);
+        myBinarySearchTree.add(60);
+        myBinarySearchTree.add(110);
+        myBinarySearchTree.add(250);
+        myBinarySearchTree.add(275);
+        myBinarySearchTree.print();
 }
