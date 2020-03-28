7ed3e5d0e84b2f6ceda34a627ea85b04f1829162
prashantvermacs18 <prashant.verma_cs18@gla.ac.in>
2020-03-28 19:24:46 +0530

    create private fields in Node<E> class

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -6,6 +6,16 @@
  */
 package problem3.node;
 
-public class Node<E>
-{
+public class Node<E> {
+    private E data;
+    private Node<E> next;
+
+    public Node(E data) {
+        this.data = data;
+    }
+
+    public Node(E data, Node<E> next) {
+        this.data = data;
+        this.next = next;
+    }
 }
