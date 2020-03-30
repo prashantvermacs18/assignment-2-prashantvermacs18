8fd0097e2f45db26b03782934ee3f81dd058f0b3
prashantvermacs18 <prashant.verma_cs18@gla.ac.in>
2020-03-30 11:42:08 +0530

    create object of MyBinarySearchTree

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -7,5 +7,16 @@
 package problem4.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
+import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
+import problem4.myqueue.MyQueue;
+import java.util.Scanner;
+
 public class MyMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter elements which are added in binary search tree");
+        String s = sc.nextLine();
+        String[] ar = s.split(" ");
+        MyBinarySearchTree<Integer> myBinarySearchTree = new MyBinarySearchTree<>();
 }
