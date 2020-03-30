7952c94dcb51f8cfbda6c011ba97c1245a32372f
prashantvermacs18 <prashant.verma_cs18@gla.ac.in>
2020-03-30 11:45:18 +0530

    store all the preOrder successor element in Queue

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -19,4 +19,29 @@ public class MyMain {
         String s = sc.nextLine();
         String[] ar = s.split(" ");
         MyBinarySearchTree<Integer> myBinarySearchTree = new MyBinarySearchTree<>();
+        for (String s1 : ar) {
+            myBinarySearchTree.add(Integer.parseInt(s1));
+        }
+        MyQueue<Integer> myQueue = new MyQueue<>();
+        System.out.println("Nodes After traversing in Pre Order :");
+        MyQueue<Integer> myQueue1 = traversePreOrder(myBinarySearchTree.getRoot(), myQueue);
+        System.out.println();
+        System.out.println("Queue of Pre Order Successor :");
+        MyQueue<Integer> myQueue2 = getQueuePreOrderSuccessor(myQueue1);
+        System.out.println(myQueue2);
+    }
+    public static MyQueue traversePreOrder(TreeNode<Integer> currentNode, MyQueue<Integer> myQueue) {
+        if (currentNode != null) {
+            myQueue.enqueue(currentNode.getData());
+            System.out.print(currentNode.getData() + " ");
+            traversePreOrder(currentNode.getLeftChild(), myQueue);
+            traversePreOrder(currentNode.getRightChild(), myQueue);
+        }
+        return myQueue;
+    }
+
+    public static MyQueue getQueuePreOrderSuccessor(MyQueue myQueue) {
+        myQueue.dequeue();
+        return myQueue;
+    }
 }
