e32eb8883bcc2ecda6c228a6fcf748c7494cb053
prashantvermacs18 <prashant.verma_cs18@gla.ac.in>
2020-03-30 12:05:41 +0530

    override the toString() method

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -13,4 +13,30 @@ public class Student {
 
     public int getBackLogCounter() {
         return backLogCounter;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "---------------------------------------" + "\n" +
+                "Name -- " + getName() + "\n" +
+                "Roll Number -- " + getRollNumber() + "\n" +
+                "Back Log Value -- " + getBackLogCounter() + "\n" +
+                "---------------------------------------";
+    }
 }
