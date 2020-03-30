554ea0ee75efeee46596bdfe0b3a8d6e607bea69
prashantvermacs18 <prashant.verma_cs18@gla.ac.in>
2020-03-30 11:48:17 +0530

    provide methods to define by extending class

--- src/problem5/adt/CircularQueueADT.java
+++ src/problem5/adt/CircularQueueADT.java
@@ -1,4 +1,19 @@
 package problem5.adt;
 
 public interface CircularQueueADT<E> {
+    //a method to add new element at rear
+    public void enqueue(E data);
+
+    //a method to remove element from front of queue
+    public E dequeue();
+
+    //a method to get front element of queue
+    public E peek();
+
+    // a method to check whether queue is empty
+    public boolean isEmpty();
+
+    // to get size of queue
+    public int getSize();
+}
 }
