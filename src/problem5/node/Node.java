ae969c6bb20494db61b7f369507cbc27ece3acaf
prashantvermacs18 <prashant.verma_cs18@gla.ac.in>
2020-03-30 12:09:05 +0530

    define contructor, getter and setter

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -6,5 +6,26 @@
  */
 package problem5.node;
 // to define node properties
-public class Node<E> {
+import problem5.student.Student;
+
+public class Node {
+    private Student student;
+    private Node next;
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
 }
