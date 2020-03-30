af40280e6e2cc83eb52f917e5448606dc1006003
prashantvermacs18 <prashant.verma_cs18@gla.ac.in>
2020-03-30 12:29:49 +0530

    end commit

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -6,5 +6,47 @@
  */
 package problem5.main;
 //executable class
+import problem5.circularqueue.MyCircularQueue;
+import problem5.node.Node;
+import problem5.student.Student;
+import java.util.Scanner;
 public class MyMain {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        MyCircularQueue circularQueue = new MyCircularQueue();
+
+        System.out.println("Enter number of records you wants to enter");
+        int numberOfRecords = scan.nextInt();
+        while (numberOfRecords != 0) {
+            Student student = new Student();
+            Node node = new Node();
+
+            System.out.println("Enter name");
+            student.setName(scan.next().trim());
+            System.out.println("Enter roll number");
+            student.setRollNumber(scan.nextInt());
+            System.out.println("Enter backlogs");
+            int backLogs = scan.nextInt();
+            if (backLogs > 2) {
+                System.out.println("This student is not allowed");
+                continue;
+            }
+            student.setBackLogCounter(backLogs);
+
+            node.setStudent(student);
+
+            circularQueue.enQueue(node);
+
+            numberOfRecords--;
+        }
+        System.out.println("Data before deleting records");
+        circularQueue.traverseQueue();
+
+        System.out.println("Removing zero backlog students");
+        circularQueue.removeZeroBacklogRecords();
+
+        System.out.println("Data after deleting records");
+        circularQueue.traverseQueue();
+    }
+
 }
